288c42d4f1f6b919f955b2c59514c170be28aff6
SyN Max Technology <63320165+SyNmaxtechnology@users.noreply.github.com>
2024-05-14 17:43:46 -0600

    cambios 1305

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',loadChildren: () => import("./web/web.module").then(w => w.WebModule)},
+    {path: '',loadChildren: () => import("./auth/auth.module").then(w => w.AuthModule)},
+    {path: '',loadChildren: () => import("./admin/admin.module").then(w => w.AdminModule)},
+];
